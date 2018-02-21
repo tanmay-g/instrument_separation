e40408612c501fe942a934a1e83b6f3d5aa5cdb3
Tanmay <tanmay.godbole@student.tut.fi>
2018-02-22 00:13:18 +0200

    Added step 3 and 4

--- separation.py
+++ separation.py
@@ -14,29 +14,61 @@ def separate_instruments(file_name = "./inputs/rhythm_birdland.wav"):
     winlen = 1024
 
     # Step 1: Generate STFT
+
+    # Have used terminology from the paper
+    # h and i represent indices of frequency and time bins
     h, i, F = stft(x=x, fs=fs, window='hann', nperseg=winlen, noverlap=int(winlen / 2),
                    nfft=winlen, detrend=False, return_onesided=True, padded=True, axis=-1)
 
-    # Plot spectrogram
-    plt.pcolormesh(i, h, np.abs(F))
-    plt.title('STFT Magnitude')
-    plt.ylabel('Frequency [Hz]')
-    plt.xlabel('Time [sec]')
-    plt.show()
+    # # Plot spectrogram
+    # plt.pcolormesh(i, h, np.abs(F))
+    # plt.title('STFT Magnitude')
+    # plt.ylabel('Frequency [Hz]')
+    # plt.xlabel('Time [sec]')
+    # plt.show()
 
     # Step 2: Calculate a range-compressed version of the power spectrogram
     gamma = 0.3
     W = np.power(np.abs(F), 2 * gamma)
 
-    # Plot spectrogram
-    plt.pcolormesh(i, h, W)
-    plt.title('Power spectrogram')
-    plt.ylabel('Frequency [Hz]')
-    plt.xlabel('Time [sec]')
-    plt.show()
+    # # Plot spectrogram
+    # plt.pcolormesh(i, h, W)
+    # plt.title('Power spectrogram')
+    # plt.ylabel('Frequency [Hz]')
+    # plt.xlabel('Time [sec]')
+    # plt.show()
+
+    # Step 3: Initialise
+    k_max = 50
+    H = P = 0.5 * W
+    alpha = 0.3
 
 
+    for k in range(k_max):
+        # H_new = np.empty_like(H)
+        # P_new = np.empty_like(P)
+        delta = term_1 = term_2 = np.empty_like(H)
 
+        # Step 4: Calculate update variable delta
+        for i_iter in range(1, np.shape(H)[1]-1):
+            term_1[:, i_iter] = alpha * ((H[:, i_iter-1] + H[:, i_iter+1] - (2 * H[:, i_iter])) / 4)
+        term_1[:, 0] = alpha * ((H[:, 1] - H[:, 0]) / 2)
+        term_1[:, -1] = alpha * ((H[:, -2] - H[:, -1]) / 2)
+
+        for h_iter in range(1, np.shape(H)[0]-1):
+            term_2[h_iter, :] = (1 - alpha) * ((P[h_iter-1, :] + P[h_iter+1, :] - (2 * P[h_iter, :])) / 4)
+        term_2[0, :] = (1 - alpha) * ((P[1, :] - P[0, :]) / 2)
+        term_2[-1, :] = (1 - alpha) * ((P[-2, :] - P[-1, :]) / 2)
+
+        delta = term_1 - term_2
+
+        # Step 5: Update H and P
+
+        # Step 6: Increment k (automatically through loop)
+
+    # Step 7: Binarize the separation result
+
+    # Step 8: Generate separate waveforms
 
 if __name__ == '__main__':
     print("Beginning run")
